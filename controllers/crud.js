1e4f4cdb677e41f8f4488287477a2750f08300fc
Jhon FM <jfranciaminaya@gmail.com>
2025-04-15 10:26:46 -0500

    Crud básico NodeJS Express terminado

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,23 +1,37 @@
 const conexion = require('../database/db');
 
 exports.save = (req, res) => {
-  //Acceder a los datos enviados (vista)
   const tipo = req.body.tipo;
   const marca = req.body.marca;
   const color = req.body.color;
 
-  //NOTA: Se debe especificar cómo capturar los datos de los <form></form> app.js
-  //Evidencia de captura de datos
+  //Evidenciamos si llegan los datos
   //console.log(tipo, marca, color);
-
-  conexion.query(`INSERT INTO vehiculos SET ?`, 
+  conexion.query("INSERT INTO vehiculos SET ?", 
     { tipo: tipo, marca: marca, color: color }, (error, results) => {
       if (error){
         console.log(error);
       }else{
-        //Redireccionar
-        //console.log("Grabado correctamente");
+        //redireccionando
         res.redirect('/');
       }
     });
 };
+
+exports.update = (req, res) => {
+  const id = req.body.id;
+  const tipo = req.body.tipo;
+  const marca = req.body.marca;
+  const color = req.body.color;
+
+  conexion.query("UPDATE vehiculos SET ? WHERE id = ?", 
+    [{tipo: tipo, marca: marca, color: color}, id], (error, results) => {
+      if (error){
+        console.log(error);
+      }else{
+        res.redirect('/');
+      }
+    });
+};
+
+//exports.delete
--- router.js
+++ router.js
@@ -1,29 +1,48 @@
 const express = require('express');
 const router = express.Router();
+
 const conexion = require('./database/db');
 
-//req = solicitud | res = respuesta
 router.get('/', (req, res) => {
-  //Retornamos una colección de datos. Consulta exitosa "results", falló "error"
   conexion.query("SELECT * FROM vehiculos", (error, results) => {
-    if (error){
+    if (error) {
       throw error;
-    }else{
-      //Enviamos "json" los datos al navegador
-      //res.send(results);
-      //res.render('edit', { dev: 'Jhon Francia Minaya', skill: 'Javascript', friends: ['Sergio', 'José', 'Raul'] });
-      res.render('index', { registros: results })
+    } else {
+      res.render('index', { results : results});
     }
   });
 });
 
-//Enrutador para el registro
 router.get('/create', (req, res) => {
-  res.render('create');
+  res.render('create')
 });
 
-//Acceder a toda la lógica
+//Ruta para editar los registros
+router.get('/edit/:id', (req, res) => {
+  conexion.query("SELECT * FROM vehiculos WHERE id = ?", [req.params.id], (error, results) => {
+    if (error){
+      throw error;
+    }else{
+      res.render('edit', {vehiculo: results[0]});
+    }
+  });
+})
+
+
+//Ruta para eliminar
+router.get('/delete/:id', (req, res) => {
+  conexion.query("DELETE FROM vehiculos WHERE id = ?", [ req.params.id], (error, results) => {
+    if (error){
+      throw error;
+    }else{
+      res.redirect('/');
+    }
+  });
+});
+
+//Accedemos a toda la lógica
 const crud = require('./controllers/crud');
 router.post('/save', crud.save);
+router.post('/update', crud.update);
 
 module.exports = router;
